79ae3ccfd9659f32f444511d87ba1681e8e2f139
AshishDhote30 <ashish.dhote@qualit.co.nz>
2019-12-10 15:45:33 +1300

    winium

---
+++ src/main/java/sampleTest.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.winium.DesktopOptions;
+import org.openqa.selenium.winium.WiniumDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class sampleTest {
+
+    @Test
+    public void winiumtest() throws Exception{
+
+        WiniumDriver driver = null;
+
+        String appPath = "C:/windows/system32/calc.exe";
+        DesktopOptions option = new DesktopOptions();
+        option.setApplicationPath(appPath);
+        option.setDebugConnectToRunningApp(false);
+        option.setLaunchDelay(2);
+        driver = new WiniumDriver(new URL("http://localhost:9999"), option);
+
+        Thread.sleep(5000);
+
+        driver.findElementByName("One").click();
+        driver.findElementByName("Plus").click();
+
+
+        /*  WebElement window =  driver.findElementByClassName("CalcFrame");
+
+        WebElement menuItem = window.findElement(By.id("MenuBar")).findElement(By.name("View"));
+        menuItem.click();
+
+        driver.findElementByName("Scientific").click();
+
+        window.findElement(By.id("MenuBar")).findElement(By.name("View")).click();
+        driver.findElementByName("History").click();
+
+        window.findElement(By.id("MenuBar")).findElement(By.name("View")).click();
+        driver.findElementByName("History").click();
+
+        window.findElement(By.id("MenuBar")).findElement(By.name("View")).click();
+        driver.findElementByName("Standard").click();
+
+        driver.findElementByName("4").click();
+        driver.findElementByName("Add").click();
+        driver.findElementByName("5").click();
+        driver.findElementByName("Equals").click();
+        driver.close();*/
+    }
+}
